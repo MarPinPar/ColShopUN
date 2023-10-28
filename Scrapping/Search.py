7f86f6e9a52657f12fb80020d7f58557d71355a4
Nataly Sierra Molina <ksierram@unal.edu.co>
2023-10-28 11:16:23 -0500

    Uploading

--- Scrapping/Search.py
+++ Scrapping/Search.py
@@ -28,6 +28,6 @@ chromedriver_path = '/Users/knsmolina.28/Desktop/Scrapping/chromedriver'
 output_csv_path = 'products.csv'
 
 
-product_to_search = "Iphone 14"
+product_to_search = "Computador Portátil Gamer ASUS TUF"
 result = unified_product_search(product_to_search, chromedriver_path, output_csv_path)
 print(result)
