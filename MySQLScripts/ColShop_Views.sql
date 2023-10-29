42e91a7596c5ef67b20f758b65bad1decde7980f
nikoduque-doo <98504134+nikoduque-doo@users.noreply.github.com>
2023-10-29 09:51:37 -0500

    Vista perfiles

--- MySQLScripts/ColShop_Views.sql
+++ MySQLScripts/ColShop_Views.sql
@@ -1,13 +1,31 @@
-DROP VIEW IF EXISTS myProfile;
-CREATE VIEW myProfile AS
-SELECT * FROM USUARIO WHERE us_username = SUBSTRING_INDEX(USER(), '@', 1);
+-- -----------------------------------------------------------------------
+-- CREACION VISTAS
+-- -----------------------------------------------------------------------
+
+-- Vista del perfil del propio usuario, incluye contraseña
+DROP VIEW IF EXISTS miPerfil;
+CREATE VIEW miPerfil AS
+	SELECT * FROM USUARIO WHERE us_username = SUBSTRING_INDEX(USER(), '@', 1);
+
+-- Vista del perfil de otros usuarios, no incluye contraseñas
+DROP VIEW IF EXISTS perfiles;
+CREATE VIEW perfiles AS
+	SELECT us_username, us_alias, us_correo, us_fechaReg FROM USUARIO WHERE us_username != SUBSTRING_INDEX(USER(), '@', 1);
+
+-- -----------------------------------------------------------------------
+-- CREACION ROLES
+-- -----------------------------------------------------------------------
 
 DROP ROLE IF EXISTS 'usuario'@'localhost';
 CREATE ROLE 'usuario'@'localhost';
-GRANT SELECT ON myProfile TO 'usuario'@'localhost';
+GRANT SELECT ON miPerfil TO 'usuario'@'localhost';
+GRANT SELECT ON perfiles TO 'usuario'@'localhost';
+
+-- -----------------------------------------------------------------------
+-- CREACION USUARIOS
+-- -----------------------------------------------------------------------
 
 DROP USER IF EXISTS 'juan_perez'@'localhost';
 CREATE USER 'juan_perez'@'localhost' IDENTIFIED BY 'Cl4v3#123';
-
 GRANT 'usuario'@'localhost' TO 'juan_perez'@'localhost';
 SET DEFAULT ROLE ALL TO 'juan_perez'@'localhost';
