c6a4e6199023f8d7f6d3158992c199e157e078e0
Nataly Sierra Molina <ksierram@unal.edu.co>
2023-11-19 12:21:26 -0500

    Uploading new procedures

--- MySQLScripts/ColShop_Procedures.sql
+++ MySQLScripts/ColShop_Procedures.sql
@@ -1,12 +1,24 @@
-''
-Posibles procedimientos:
+USE ColShop;
 
-- [ ]  **Procedimiento para actualizar información del producto**
-- [ ]  **Procedimiento para eliminar un producto**
-- [ ]  **Procedimiento para obtener el producto más barato**
-- [ ]  **Procedimiento para obtener el historial de precios de un producto**
-- [ ]  **Crear un procedimiento para insertar datos**
-- [ ]  **Crear un procedimiento para buscar productos**
-- [ ]  **Procedimiento para obtener los precios de un producto en diferentes tiendas**
-- [ ]  **Procedimiento para obtener productos en una tienda a partir de un rango de precios**
-''
+-- Get lowest prices of a product 
+DELIMITER $$
+
+CREATE PROCEDURE GetLowestPriceByProductName
+(
+    IN p_PartialProductName VARCHAR(45)
+)
+BEGIN
+    SELECT TI.tie_nombre, PR.pre_valor
+    FROM TIENDA TI
+    INNER JOIN PRECIO PR ON TI.tie_ID = PR.TIENDA_tie_ID
+    INNER JOIN PRODUCTO P ON PR.PRODUCTO_pro_ID = P.pro_ID
+    WHERE P.pro_nombre LIKE CONCAT('%', p_PartialProductName, '%')
+    ORDER BY PR.pre_valor ASC
+    LIMIT 1;
+END $$
+
+DELIMITER ;
+
+--  Call the lowest prices 
+CALL GetLowestPriceByProductName('Parlante JBL Go');
+
