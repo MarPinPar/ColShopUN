ac0f58a451f4d4a508737e8b0bc9cb8ba8b06f10
nikoduque-doo <98504134+nikoduque-doo@users.noreply.github.com>
2023-11-19 13:56:11 -0500

    Indices 2.0

--- MySQLScripts/ColShop_Indexes.sql
+++ MySQLScripts/ColShop_Indexes.sql
@@ -1,39 +1,21 @@
+-- 1. Index on user's alias
+CREATE INDEX idx_us_alias ON USUARIO (us_alias);
+-- Query for user alias Octavio
+SELECT SQL_NO_CACHE * FROM USUARIO
+WHERE us_alias = 'Octavio';
 
--- 1.  Query for an ADMINISTRATOR looking for the count of Registered Users per Month:
+-- 2.  Index on an user's sign-up date
 CREATE INDEX idx_us_fechaReg ON USUARIO (us_fechaReg);
+-- Query for an ADMINISTRATOR looking for the count of Registered Users per Month:
 SELECT DATE_FORMAT(us_fechaReg, '%Y-%m') AS Mes_Registro, COUNT(*) AS Total_Usuarios
 FROM 
 	USUARIO
 GROUP BY 
 	Mes_Registro;
-
--- 2. Query for a BUYER looking for products by name and brand with a price range:(2)
-CREATE INDEX idx_producto_busqueda ON PRODUCTO (pro_nombre, pro_marca, pro_ID);
-SELECT 
-	pro_nombre, pro_marca
-FROM 
-	PRODUCTO
-WHERE 
-	pro_nombre LIKE '%Apple iPhone 14%' 
-  AND 
-	pro_marca = 'Iphone'
-  AND 
-	pro_ID IN (
-		SELECT 
-			PRODUCTO_pro_ID
-		FROM 
-			PRECIO
-		WHERE 
-			pre_valor >= 4000000
-      AND 
-		pre_valor <= 6000000
-  );
   
-  
-  
-  -- 3. Query for a BUYER looking for the lowest price of a product and in which shop is it:
-CREATE INDEX idx_precio_producto_tienda ON PRECIO (PRODUCTO_pro_ID, pre_valor, TIENDA_tie_ID);
-
+  -- 3. Index on a product's id and price
+CREATE INDEX idx_precio_producto_tienda ON PRECIO (PRODUCTO_pro_ID, pre_valor);
+-- Query for a BUYER looking for the lowest price of a product and in which shop is it:
 SELECT
     MIN(pre_valor) AS Precio_Más_Bajo,
     tie_nombre AS Tienda
@@ -50,10 +32,9 @@ GROUP BY
 ORDER BY
     Precio_Más_Bajo;
     
-
-    
--- 4. Query for a BUYER looking for the latest update of a product
+-- 4. Index on a product's id, date and price prioritizing the date
 CREATE INDEX idx_precio_producto_fecha_valor ON PRECIO (PRODUCTO_pro_ID, pre_fechaHora, pre_valor);
+-- Query for a BUYER looking for the latest update of a product
 SELECT 
 	pro_nombre AS Producto, pre_fechaHora AS Ultima_Actualizacion, pre_valor AS Precio
 FROM 
@@ -67,5 +48,15 @@ ORDER BY
 LIMIT 
 	1;
 
+-- 5 Index on a product's name
+CREATE INDEX idx_producto_nombre ON PRODUCTO (pro_nombre);
+-- Query for products that dont have Iphone in their names
+SELECT * FROM PRODUCTO
+	WHERE pro_nombre NOT LIKE '%Iphone%';
 
-  
+-- 6. Index on a each product's review scores
+CREATE INDEX idx_reseña_calificacion ON RESEÑA (PRODUCTO_pro_ID, res_calificacion);
+-- Query for reviews with a score lower than 3 for a specific product
+
+ 
+SET profiling = 0;
