8424bec18ea50e3587c4e8d6bed78dfce3355a3e
Nataly Sierra Molina <ksierram@unal.edu.co>
2023-11-19 12:55:57 -0500

    Upload another procedure

--- MySQLScripts/ColShop_Procedures.sql
+++ MySQLScripts/ColShop_Procedures.sql
@@ -1,6 +1,6 @@
 USE ColShop;
 
--- Get lowest prices of a product 
+-- 1. This procedure retrieves the  lowest price of a product 
 DELIMITER $$
 
 CREATE PROCEDURE GetLowestPriceByProductName
@@ -20,5 +20,25 @@ END $$
 DELIMITER ;
 
 --  Call the lowest prices 
+
 CALL GetLowestPriceByProductName('Parlante JBL Go');
+
+
+
+
+-- 2. This procedure retrieves prices for all products that match a given search term in their names
+DELIMITER $$
+
+CREATE PROCEDURE GetPricesForProductsBySearch(IN p_SearchTerm VARCHAR(255))
+BEGIN
+    SELECT TI.tie_nombre, P.pro_nombre, PR.pre_valor, PR.pre_fechaHora
+    FROM TIENDA TI
+    INNER JOIN PRECIO PR ON TI.tie_ID = PR.TIENDA_tie_ID
+    INNER JOIN PRODUCTO P ON PR.PRODUCTO_pro_ID = P.pro_ID
+    WHERE P.pro_nombre LIKE CONCAT('%', p_SearchTerm, '%');
+END $$
+
+DELIMITER ;
+
+CALL GetPricesForProductsBySearch('iPhone 14');
 
